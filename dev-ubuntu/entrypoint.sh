c2bc320f4670846e97c03c9c3ff36005f4aa9d60
mcp <benjamin.gemmill@gmail.com>
2016-07-22 12:07:47 -0400

    docs

--- dev-ubuntu/entrypoint.sh
+++ dev-ubuntu/entrypoint.sh
@@ -11,7 +11,7 @@ chown $USER_NAME /home/$USER_NAME
 # Since su has problems starting up a shell with -c without screwing up signal handling (try quitting top with ctrl-c), we detect the docker zero-argument case and just drop the user into bash.
 # We could use gosu here, but that would both make the dockerfiles complex as well as make the images bigger.
 
-# note that docker's "zero argument" case starts up bash with sh
+# note that docker's "zero argument" case starts up the dockerfile's CMD with sh
 if [[ "$@" == "/bin/sh -c bash" ]]
 then
   # No arguments passed to docker run, so drop the user into a shell
