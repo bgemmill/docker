f0a4cc5d563f5c5ff225853840adab0e94f68fc3
mcp <benjamin.gemmill@gmail.com>
2016-07-30 13:37:40 -0400

    gksu for pycharm to handle python dependencies

--- dev-ubuntu/entrypoint.sh
+++ dev-ubuntu/entrypoint.sh
@@ -2,10 +2,15 @@
 
 # add a user to run commands with
 USER_NAME=${USER_NAME:=docker}
+USER_PASS=${USER_PASS:=docker}
+USER_GROUPS=${USER_GROUPS:=audio,sudo}
 USER_ID=${USER_ID:=1000}
-useradd -m $USER_NAME -u $USER_ID > /dev/null 2>&1
-usermod -a -G audio $USER_NAME > /dev/null 2>&1
+
+# create that user
+useradd -m $USER_NAME -u $USER_ID -G $USER_GROUPS > /dev/null 2>&1
+chpasswd <<< "$USER_NAME:$USER_PASS"
 chown $USER_NAME /home/$USER_NAME
+touch /home/$USER_NAME/.Xauthority; chown $USER_NAME /home/$USER_NAME/.Xauthority # gksu needs this file, even if empty
 
 # run commands as user
 # Since su has problems starting up a shell with -c without screwing up signal handling (try quitting top with ctrl-c), we detect the docker zero-argument case and just drop the user into bash.
